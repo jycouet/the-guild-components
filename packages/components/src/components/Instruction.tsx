342469b431d79b894ab4d9eb1161088b4f716509
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-04-06 14:01:55 +0200

    refactor instruction

--- packages/components/src/components/Instruction.tsx
+++ packages/components/src/components/Instruction.tsx
@@ -1,20 +1,57 @@
 import { FC } from 'react';
-import {
-  Container,
-  UpperLine,
-  SingleDot,
-  Inner,
-  Content,
-} from './Instruction.styles';
 
 export const Instruction: FC = ({ children }) => {
   return (
-    <Container>
-      <UpperLine />
-      <Inner>
-        <SingleDot />
-        <Content>{children}</Content>
-      </Inner>
-    </Container>
+    <div>
+      <div
+        className="
+          relative
+          mb-2
+          h-4
+          before:absolute
+          before:left-[3px]
+          before:h-full
+          before:w-1
+          before:rounded-sm
+          before:bg-cyan-500
+          before:content-['']
+        "
+      />
+      <div className="flex">
+        <div
+          className="
+            relative
+            h-2.5
+            w-2.5
+            before:absolute
+            before:top-0
+            before:left-0
+            before:h-2.5
+            before:w-2.5
+            before:rounded-full
+            before:bg-cyan-500
+            before:content-['']
+          "
+        />
+        <div
+          className="
+            relative
+            pl-4
+            pb-2.5
+            text-cyan-500
+            before:absolute
+            before:bottom-0
+            before:top-5
+            before:left-[-7px]
+            before:w-1
+            before:rounded-sm
+            before:bg-cyan-500
+            before:content-['']
+          "
+        >
+          {children}
+        </div>
+      </div>
+    </div>
   );
 };
