6d0ca48595a39e0c20094fde1efa73c149e5bddc
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-04-06 14:46:48 +0200

    remove twin, build pass

--- examples/nextjs/pages/_app.tsx
+++ examples/nextjs/pages/_app.tsx
@@ -1,23 +1,16 @@
 import { FC } from 'react';
 import { AppProps } from 'next/app';
-import {
-  Footer,
-  Header,
-  GlobalStyles,
-  Subheader,
-  ThemeProvider,
-} from '@theguild/components';
-import '@theguild/components/dist/index.css'
+import { Footer, Header, Subheader, ThemeProvider } from '@theguild/components';
+import '@theguild/components/dist/index.css';
+import '@theguild/components/dist/fonts.css';
 
 // Overwrite font example:
-// 1. Import the font
-// 2. Remove `includeFonts` from `<GlobalStyles />`
-// import '../public/styles.css';
+// 1. Remove import fonts.css
+// 2. import '../public/styles.css'
 
 const App: FC<AppProps> = ({ router, pageProps, Component }) => {
   return (
     <>
-      <GlobalStyles includeBase includeFonts />
       <ThemeProvider>
         <Header accentColor="#1cc8ee" activeLink="/open-source" themeSwitch />
         <Subheader
